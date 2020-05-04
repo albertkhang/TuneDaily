2eb0d089293b50e7a40e7186ad6976242bead30a
Albert Khang <albertkhang2017@gmail.com>
2020-05-04 18:14:16 +0700

    create SettingsActivity, add animation MainActivity and SettingsActivity

--- app/src/main/java/com/albertkhang/tunedaily/activities/SettingsActivity.java
+++ app/src/main/java/com/albertkhang/tunedaily/activities/SettingsActivity.java
@@ -2,37 +2,40 @@ package com.albertkhang.tunedaily.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.albertkhang.tunedaily.R;
 
-public class MainActivity extends AppCompatActivity {
-    private ImageView imgSettings;
+public class SettingsActivity extends AppCompatActivity {
+    private ImageView imgBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
 
         addControl();
         addEvent();
     }
 
     private void addControl() {
-        imgSettings = findViewById(R.id.imgSettings);
+        imgBack = findViewById(R.id.imgBack);
     }
 
     private void addEvent() {
-        imgSettings.setOnClickListener(new View.OnClickListener() {
+        imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.animation_start_enter, R.anim.animation_start_leave);
+                finish();
             }
         });
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.animation_finish_enter, R.anim.animation_finish_leave);
     }
 }
