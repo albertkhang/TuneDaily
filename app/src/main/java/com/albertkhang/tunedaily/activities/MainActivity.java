74c5301eb38d9224579225351428b8efc405790a
Albert Khang <albertkhang2017@gmail.com>
2020-05-04 10:11:47 +0700

    add SplashScreen, add app icon, add MainActivity

--- app/src/main/java/com/albertkhang/tunedaily/activities/MainActivity.java
+++ app/src/main/java/com/albertkhang/tunedaily/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.albertkhang.tunedaily;
+package com.albertkhang.tunedaily.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.albertkhang.tunedaily.R;
 
 public class MainActivity extends AppCompatActivity {
 
