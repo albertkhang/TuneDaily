4862c0f7563fb8fcbe0b8fdd1af1009c5405e353
Albert Khang <albertkhang2017@gmail.com>
2020-05-11 10:11:23 +0700

    remove favourited, listened

--- app/src/main/java/com/albertkhang/tunedaily/fragments/FragmentFullPlayer.java
+++ app/src/main/java/com/albertkhang/tunedaily/fragments/FragmentFullPlayer.java
@@ -1,29 +1,84 @@
 package com.albertkhang.tunedaily.fragments;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.albertkhang.tunedaily.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+
 public class FragmentFullPlayer extends Fragment {
-
-    public FragmentFullPlayer() {
-        // Required empty public constructor
-    }
-
+    private static final String PATH = "https://firebasestorage.googleapis.com/v0/b/dynamic-density-271310.appspot.com/o/songs%2FThuanTheoYTroi-BuiAnhTuan-6150266.mp3?alt=media&token=1a3d42f0-27b0-449c-84a4-89475fccf2e6";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_full_player, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addControl();
+        addEvent();
+    }
+
+    private void addControl() {
+        testMetaDataFireBase();
+    }
+
+    private void addEvent() {
+
+    }
+
+    private void testMetaDataFireBase() {
+        Log.d("MetaData", "testMetaDataFireBase");
+
+//        FirebaseStorage storage = FirebaseStorage.getInstance();
+//        StorageReference storageRef = storage.getReference();
+//
+//        storageRef.child("songs/ThuanTheoYTroi-BuiAnhTuan-6150266.mp3").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//            @Override
+//            public void onSuccess(Uri uri) {
+//                Log.d("MetaData", "onSuccess");
+//                Log.d("MetaData", "getPath " + uri.getPath());
+//
+//                MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(), uri);
+//                Log.d("MetaData", "getDuration " + mediaPlayer.getDuration());
+//
+//
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception exception) {
+//                Log.d("MetaData", "onFailure: " + exception);
+//            }
+//        });
     }
 }
